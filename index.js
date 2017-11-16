a43f6813167d90912bc2e733f3a48e23c5aaacd5
danl411412 <daan.b.lee@gmail.com>
2017-11-16 05:29:48 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,3 +4,5 @@ const currentUser = "Dan";
 const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
 
 const excitedWelcomeMessage = `WELCOME TO FLATBOOK, ${currentUser.toUpperCase()}!`;
+
+const shortGreeting = `Welcome, ${currentUser.slice(0,1)}!`;
